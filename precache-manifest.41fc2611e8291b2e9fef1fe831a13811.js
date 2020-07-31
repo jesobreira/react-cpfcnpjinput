7361723dbc5d7feccfa5bc9e4cafd327d67bddf5
Jefrey <jefrey.santos@capitual.io>
2020-07-31 13:07:17 -0300

    Updates

---
+++ precache-manifest.41fc2611e8291b2e9fef1fe831a13811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f480eb27eba7a0a8c81f71dc827f4686",
+    "url": "./index.html"
+  },
+  {
+    "revision": "570caacd22931163671b",
+    "url": "./static/css/main.b904b002.chunk.css"
+  },
+  {
+    "revision": "bbd5ddbb8612dda0680a",
+    "url": "./static/js/2.4e9dbcdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4e9dbcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570caacd22931163671b",
+    "url": "./static/js/main.29ced18c.chunk.js"
+  },
+  {
+    "revision": "75e6d7cc8ce47942ee91",
+    "url": "./static/js/runtime-main.466b02b9.js"
+  }
+]);
